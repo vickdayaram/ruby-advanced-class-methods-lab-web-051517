64c93764e4df99da740df06a2f2f8fa97fbc81fa
github username <github email address>
2017-05-18 13:27:42 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,12 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  def initialize(*name)
+    @name == name
+  end
 
   def self.all
     @@all
@@ -9,5 +15,71 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.create
+     new_song = Song.new
+     @@all << new_song
+     new_song
+  end
+
+  def self.new_by_name(name)
+     new_song = Song.new(name)
+     new_song.name = name
+     new_song
+  end
+
+  def self.create_by_name(name)
+     new_song = Song.new(name)
+     @@all << new_song
+     new_song.name = name
+     new_song
+  end
+
+  def self.find_by_name(name)
+     @@all.each do |song_name|
+       if name == song_name.name
+         return song_name
+       end
+     end
+  end
+
+  def self.find_or_create_by_name(name)
+     if !@@all.include?(name)
+       self.create_by_name(name)
+     end
+  end
+
+  def self.alphabetical
+      @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(file_name)
+      file_split = file_name.split('.')
+      file_split2 = file_split[0].split(" - ")
+
+      song_name = file_split2[1]
+      artist = file_split2[0]
+
+      new_song = self.create_by_name(song_name)
+      new_song.artist_name = artist
+      new_song
+  end
+
+  def self.create_from_filename(file_name)
+      file_split = file_name.split('.')
+      file_split2 = file_split[0].split(" - ")
+
+      song_name = file_split2[1]
+      artist = file_split2[0]
+
+      new_song = self.create_by_name(song_name)
+      @@all << new_song
+      new_song.artist_name = artist
+      new_song
+   end
+
+   def self.destroy_all
+     @@all.clear 
+   end
 
 end
